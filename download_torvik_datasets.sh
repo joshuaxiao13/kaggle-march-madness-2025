63931f3f81c4ad53a13588b0646fd945e69f3c9a
Joshua Xiao <joshuaxiao13@gmail.com>
2025-03-07 23:34:08 +0000

    add command to download torvik wins against top 30

--- download_torvik_datasets.sh
+++ download_torvik_datasets.sh
@@ -4,7 +4,11 @@ header_file_path="datasets/torvik/torvik_team_header.csv"
 # 2024 Selection Day: 2024-03-18
 
 cp ${header_file_path} ${mens_output_dir}/team_2024.csv 
-
 wget -qO- \
 	"https://barttorvik.com/trank.php?year=2024&sort=&hteam=&t2value=&conlimit=All&state=All&begin=20231101&end=20240318&top=0&revquad=0&quad=5&venue=All&type=All&mingames=0&csv=1" \
 	| sed --expression='1d' >> ${mens_output_dir}/team_2024.csv
+
+cp ${header_file_path} ${mens_output_dir}/top30_wins_2024.csv
+wget -qO- \
+	"https://barttorvik.com/trank.php?year=2024&sort=&hteam=&t2value=&conlimit=All&state=All&begin=20231101&end=20240318&top=30&revquad=0&quad=5&venue=All&type=All&mingames=0&csv=1" \
+	| sed --expression='1d' >> ${mens_output_dir}/top30_wins_2024.csv
